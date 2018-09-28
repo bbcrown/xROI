68a64ad044cf6b6552170493c8e319a14c9ca1b4
bnasr <bijan.s.nasr@gmail.com>
2018-09-27 17:13:25 -0700

    cleanup built

--- toCRAN/R/zzz.R
+++
@@ -1,8 +0,0 @@
-globalVariables(names = c('Year','Month', 'Day', 'Hour', 'Minute', 'Second',
-                          'HHMMSS','DOY', 'Date', 'DateTime', 'YearDOY',
-                          'ID', 'Label', 'x', 'y', 'z', '.', 'archive',
-                          'mainDataPath', 'middayListPath', 'newpath', 'path', 'filenames',
-                          'band', 'blackness',  'conT', 'dukehw', 'empty',
-                          'b5', 'b10', 'b25', 'b75', 'b90', 'b95', 'blue',
-                          'g5', 'g10', 'g25', 'g75', 'g90', 'g95', 'green',
-                          'r5', 'r10', 'r25', 'r75', 'r90', 'r95', 'red'))
---
+++ vignettes/Launch-xROI.R
@@ -0,0 +1,21 @@
+## ---- echo=TRUE, eval=FALSE----------------------------------------------
+#  
+#  utils::install.packages('xROI')
+#  
+
+## ---- echo=TRUE, eval=FALSE----------------------------------------------
+#  
+#  devtools::install_github("bnasr/xROI").
+#  
+
+## ---- echo=TRUE, eval=FALSE----------------------------------------------
+#  
+#  library(xROI)
+#  Launch()
+#  
+
+## ---- echo=TRUE, eval=FALSE----------------------------------------------
+#  
+#  Rscript -e “xROI::Launch(Interactive = TRUE)”
+#  
+
